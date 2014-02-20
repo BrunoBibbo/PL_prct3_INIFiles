280eed5a971814843bf6942f289e68f136005081
juanma3108 <alu0100597929@ull.edu.es>
2014-02-20 20:35:26 +0000

    añadidos mas tests

--- tests/tests.js
+++ tests/tests.js
@@ -3,4 +3,24 @@ var assert = chai.assert;
 suite('Pruebas para ficheros INI', function() {
     test('Comprobando correcto funcionamiento', function() {
     });
+
+    test('Espacios en blanco', function() {
+        var token = lexer('   ');
+		assert.equal(token[0].type,'blanks');
+    });
+
+    test('Asignacion', function() {
+        var token =lexer('animal = gato');
+        assert.equal(token[0].type,'nameEqualValue');
+    });
+
+    test('Comentario', function() {
+        var token = lexer('; comentario de prueba');
+		assert.equal(token[0].type,'comments');
+    });
+
+    test('Error', function() {
+        var token = lexer('@#!!?3');
+		assert.equal(token[0].type,'error');
+    });
 });
